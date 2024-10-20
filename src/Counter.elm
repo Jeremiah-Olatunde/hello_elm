d268d2bd95b7d72290573669f8b6e4fc59a329e7
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-20 13:51:37 +0100

    feat: add simple counter

---
+++ src/Counter.elm
@@ -0,0 +1,29 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, div, text, button)
+import Html.Events exposing (onClick)
+
+main =
+  Browser.sandbox { view = view, update = update, init = init }
+
+type alias Model = Int
+
+init : Model
+init = 0
+
+type Message = Increment | Decrement
+
+update : Message -> Model -> Model
+update message model = 
+  case message of
+    Increment -> model + 1
+    Decrement -> model - 1
+
+view: Model -> Html Message
+view model =
+    div [] 
+      [ button [ onClick Decrement ] [ text "-" ]
+      , div [] [ text (String.fromInt model) ]
+      , button [ onClick Increment ] [ text "+" ] 
+    ]
