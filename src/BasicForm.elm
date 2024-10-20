1bd19265d2b314d0332c8a0a8dd6f3b7a54c336b
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-20 14:57:53 +0100

    feat(basic_form wip): add submit functionality

--- src/BasicForm.elm
+++ src/BasicForm.elm
@@ -1,28 +1,35 @@
 module BasicForm exposing (main)
 
 import Browser
-import Html exposing (Html, label, input, text, form, button)
-import Html.Attributes exposing (id, for, value, placeholder, style)
-import Html.Events exposing (onInput)
+import Html exposing (Html, label, input, text, form, button, div)
+import Html.Attributes exposing (id, for, value, placeholder, style, type_)
+import Html.Events exposing (onInput, onSubmit, onClick)
 
 main = 
   Browser.sandbox { view = view, update = update, init = init }
 
 
-type alias Model = { username: String, password: String }
+type alias FormData = { username: String, password: String }
+type Model = Success | Form FormData
 
 init: Model
-init = { username = "", password = "" }
+init = Form { username = "", password = "" }
 
 
-type Message = UpdateUsername String | UpdatePassword String
+type Message = UpdateUsername String | UpdatePassword String | SubmitForm
 
 update :  Message -> Model -> Model
 update message model =
   case message of
-    UpdateUsername username -> { model | username = username }
-    UpdatePassword password -> { model | password = password }
-
+    SubmitForm -> Success
+    UpdateUsername username -> 
+      case model of 
+        Form formData -> Form { formData | username = username }
+        Success -> Success
+    UpdatePassword password ->
+      case model of 
+        Form formData -> Form { formData | password = password }
+        Success -> Success      
 
 styleForm = 
   [ style "display" "flex" 
@@ -44,33 +51,37 @@ styleButton =
 
 view : Model ->  Html Message
 view model =
-  form 
-    ([] ++ styleForm) 
-    [ label 
-        [ for "username" ] 
-        [ text "username" ]
-    , input
-        (
-          styleInput ++ 
-          [ id "username" 
-          , value (.username model)
-          , onInput UpdateUsername 
-          , placeholder "enter username"
-          ]
-        ) 
-         []
-    , label 
-        [ for "password"]  
-        [ text "password" ]
-    , input 
-        (
-          styleInput ++ 
-          [ id "password" 
-          , value (.password model)
-          , onInput UpdatePassword 
-          , placeholder "enter password"
-          ]
-        ) 
-        []
-    , button ([] ++ styleButton) [ text "submit" ]
-    ]
+  case model of 
+    Success -> div [] [ text "SUCCESS" ]
+    Form formData -> 
+      form 
+        ([] ++ styleForm) 
+        [ label 
+            [ for "username" ] 
+            [ text "username" ]
+        , input
+            (
+              styleInput ++ 
+              [ id "username" 
+              , value (.username formData)
+              , onInput UpdateUsername 
+              , placeholder "enter username"
+              ]
+            ) 
+             []
+        , label 
+            [ for "password"]  
+            [ text "password" ]
+        , input 
+            (
+              styleInput ++ 
+              [ id "password" 
+              , value (.password formData)
+              , onInput UpdatePassword 
+              , placeholder "enter password"
+              ]
+            ) 
+            []
+        --, input ([ onSubmit SubmitForm, value "submit", type_ "submit" ] ++ styleButton) []
+        , button ([ onClick SubmitForm ] ++ styleButton) [ text "submit" ]
+        ]
