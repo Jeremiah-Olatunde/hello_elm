fa7e2ce3616817e856d3a340e1ab9a005936c6b2
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-25 15:04:52 +0100

    refactor: formatting

--- src/BasicForm.elm
+++ src/BasicForm.elm
@@ -1,87 +1,113 @@
 module BasicForm exposing (main)
 
 import Browser
-import Html exposing (Html, label, input, text, form, button, div)
-import Html.Attributes exposing (id, for, value, placeholder, style, type_)
-import Html.Events exposing (onInput, onSubmit, onClick)
-
-main = 
-  Browser.sandbox { view = view, update = update, init = init }
+import Html exposing (Html, button, div, form, input, label, text)
+import Html.Attributes exposing (for, id, placeholder, style, type_, value)
+import Html.Events exposing (onClick, onInput, onSubmit)
 
 
-type alias FormData = { username: String, password: String }
-type Model = Success | Form FormData
-
-init: Model
-init = Form { username = "", password = "" }
+main =
+    Browser.sandbox { view = view, update = update, init = init }
 
 
-type Message = UpdateUsername String | UpdatePassword String | SubmitForm
+type alias FormData =
+    { username : String, password : String }
 
-update :  Message -> Model -> Model
+
+type Model
+    = Success
+    | Form FormData
+
+
+init : Model
+init =
+    Form { username = "", password = "" }
+
+
+type Message
+    = UpdateUsername String
+    | UpdatePassword String
+    | SubmitForm
+
+
+update : Message -> Model -> Model
 update message model =
-  case message of
-    SubmitForm -> Success
-    UpdateUsername username -> 
-      case model of 
-        Form formData -> Form { formData | username = username }
-        Success -> Success
-    UpdatePassword password ->
-      case model of 
-        Form formData -> Form { formData | password = password }
-        Success -> Success      
+    case message of
+        SubmitForm ->
+            Success
 
-styleForm = 
-  [ style "display" "flex" 
-  , style "flex-direction" "column"
-  , style "width" "400px"
-  , style "margin" "auto"
-  , style "padding" "20px"
-  ] 
+        UpdateUsername username ->
+            case model of
+                Form formData ->
+                    Form { formData | username = username }
+
+                Success ->
+                    Success
+
+        UpdatePassword password ->
+            case model of
+                Form formData ->
+                    Form { formData | password = password }
+
+                Success ->
+                    Success
+
+
+styleForm =
+    [ style "display" "flex"
+    , style "flex-direction" "column"
+    , style "width" "400px"
+    , style "margin" "auto"
+    , style "padding" "20px"
+    ]
+
 
 styleInput =
-  [ style "margin" "10px 0"
-  , style "padding" "10px 5px"
-  ]
+    [ style "margin" "10px 0"
+    , style "padding" "10px 5px"
+    ]
 
-styleButton = 
-  [ style "padding" "10px"
-  , style "text-transform" "uppercase"
-  ]
 
-view : Model ->  Html Message
+styleButton =
+    [ style "padding" "10px"
+    , style "text-transform" "uppercase"
+    ]
+
+
+view : Model -> Html Message
 view model =
-  case model of 
-    Success -> div [] [ text "SUCCESS" ]
-    Form formData -> 
-      form 
-        ([] ++ styleForm) 
-        [ label 
-            [ for "username" ] 
-            [ text "username" ]
-        , input
-            (
-              styleInput ++ 
-              [ id "username" 
-              , value (.username formData)
-              , onInput UpdateUsername 
-              , placeholder "enter username"
-              ]
-            ) 
-             []
-        , label 
-            [ for "password"]  
-            [ text "password" ]
-        , input 
-            (
-              styleInput ++ 
-              [ id "password" 
-              , value (.password formData)
-              , onInput UpdatePassword 
-              , placeholder "enter password"
-              ]
-            ) 
-            []
-        --, input ([ onSubmit SubmitForm, value "submit", type_ "submit" ] ++ styleButton) []
-        , button ([ onClick SubmitForm ] ++ styleButton) [ text "submit" ]
-        ]
+    case model of
+        Success ->
+            div [] [ text "SUCCESS" ]
+
+        Form formData ->
+            form
+                ([] ++ styleForm)
+                [ label
+                    [ for "username" ]
+                    [ text "username" ]
+                , input
+                    (styleInput
+                        ++ [ id "username"
+                           , value (.username formData)
+                           , onInput UpdateUsername
+                           , placeholder "enter username"
+                           ]
+                    )
+                    []
+                , label
+                    [ for "password" ]
+                    [ text "password" ]
+                , input
+                    (styleInput
+                        ++ [ id "password"
+                           , value (.password formData)
+                           , onInput UpdatePassword
+                           , placeholder "enter password"
+                           ]
+                    )
+                    []
+
+                --, input ([ onSubmit SubmitForm, value "submit", type_ "submit" ] ++ styleButton) []
+                , button ([ onClick SubmitForm ] ++ styleButton) [ text "submit" ]
+                ]
--- src/Counter.elm
+++ src/Counter.elm
@@ -1,29 +1,42 @@
 module Counter exposing (..)
 
 import Browser
-import Html exposing (Html, div, text, button)
+import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
-main =
-  Browser.sandbox { view = view, update = update, init = init }
 
-type alias Model = Int
+main =
+    Browser.sandbox { view = view, update = update, init = init }
+
+
+type alias Model =
+    Int
+
 
 init : Model
-init = 0
+init =
+    0
 
-type Message = Increment | Decrement
+
+type Message
+    = Increment
+    | Decrement
+
 
 update : Message -> Model -> Model
-update message model = 
-  case message of
-    Increment -> model + 1
-    Decrement -> model - 1
+update message model =
+    case message of
+        Increment ->
+            model + 1
 
-view: Model -> Html Message
+        Decrement ->
+            model - 1
+
+
+view : Model -> Html Message
 view model =
-    div [] 
-      [ button [ onClick Decrement ] [ text "-" ]
-      , div [] [ text (String.fromInt model) ]
-      , button [ onClick Increment ] [ text "+" ] 
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (String.fromInt model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
